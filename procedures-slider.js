a5e82214bccb8826346a1d48af334d4cf96f5e2e
Vrybalcenko <165685927+Vrybalcenko@users.noreply.github.com>
2024-12-11 13:56:42 +0200

    Add files via upload

---
+++ procedures-slider.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidersPerViev: 1,
+    breakpoints: {
+    550: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    }
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+
+  navigation: {
+    nextEl: '.button-cards.next',
+    prevEl: '.button-cards.prev',
+  },
+
+});
+
